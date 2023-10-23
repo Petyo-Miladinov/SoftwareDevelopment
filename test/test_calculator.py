e2e4130f2294f7c4ea391de14c503606e968d2c6
Petyo-Miladinov <petyo.p.miladinov.2019@elsys-bg.org>
2023-10-23 10:02:53 +0300

    Commit

---
+++ test/test_calculator.py
@@ -0,0 +1,83 @@
+# tests/test_calculator.py
+from unittest import mock, TestCase 
+# from calculator.calculator import Calculator
+import sys
+import os
+
+# Insert the path to the directory containing the calculator module
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from calculator.calculator import Calculator
+
+import unittest 
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        calc = Calculator()
+        result = calc.add(5, 3)
+        self.assertEqual(result, 8)
+
+    def test_add_negative(self):
+        calc = Calculator()
+        result = calc.add(-5, 3)
+        self.assertEqual(result, -2)
+
+    def test_subtract(self):
+        calc = Calculator()
+        result = calc.subtract(10, 5)
+        self.assertEqual(result, 5)
+
+    def test_subtract_negative(self):
+        calc = Calculator()
+        result = calc.subtract(-5, 3)
+        self.assertEqual(result, -8)
+
+    def test_multiply(self):
+        calc = Calculator()
+        result = calc.multiply(4, 6)
+        self.assertEqual(result, 24)
+
+    def test_multiply_negative(self):
+        calc = Calculator()
+        result = calc.multiply(-5, 3)
+        self.assertEqual(result, -15)
+
+    def test_divide(self):
+        calc = Calculator()
+        result = calc.divide(10, 2)
+        self.assertEqual(result, 5.0)
+
+    def test_divide_by_zero(self):
+        calc = Calculator()
+        with self.assertRaises(ValueError) as context:
+            calc.divide(5, 0)
+        
+        self.assertEqual(str(context.exception), "Cannot divide by zero")
+
+
+    def test_operate_from_input(self): 
+        with mock.patch("builtins.input", return_value="5 + 3"):
+            with mock.patch("builtins.print") as mock_print:
+                Calculator.operate_from_input()
+                mock_print.assert_called_with(8.0)
+        with mock.patch("builtins.input", return_value="5 - 3"):
+            with mock.patch("builtins.print") as mock_print:
+                Calculator.operate_from_input()
+                mock_print.assert_called_with(2.0) 
+        with mock.patch("builtins.input", return_value="5 * 3"):
+            with mock.patch("builtins.print") as mock_print:
+                Calculator.operate_from_input()
+                mock_print.assert_called_with(15.0)
+        with mock.patch("builtins.input", return_value="5 / 3"):
+            with mock.patch("builtins.print") as mock_print:
+                Calculator.operate_from_input()
+                mock_print.assert_called_with(1.6666666666666667)
+        # with mock.patch("builtins.input", return_value="5 & 3"):
+        #     with mock.patch("builtins.print") as mock_print:
+        #         with self.assertRaises(ValueError) as context:
+        #             Calculator.operate_from_input()
+        #         self.assertEqual(str(context.exception), "Invalid operator. Please use +, -, * or /.")
+
+
+if __name__ == '__main__':
+    unittest.main()
